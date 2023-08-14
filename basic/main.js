cf057a150c9b8ddd9c00e2cefe8888c2593642d3
SeungYn <kwls0407@gmail.com>
2023-08-14 11:36:59 +0900

    feat: 회전하는 정육면체

--- basic/main.js
+++ basic/main.js
@@ -1,1 +1,33 @@
 import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+// 첫 번째 파리미터 시야 범위 각도
+// 두 번째 파라미터 비율
+// 세,네 번째 절단면인데 near 보다 가깜고 far보다 더 멀리있으면 렌더링 되지 않음
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 'blue' });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
