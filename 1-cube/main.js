593d83b946574861d9d84e44e32e709fc7c0e48e
SeungYn <kwls0407@gmail.com>
2023-08-19 09:14:08 +0900

    part-3-2-5 mesh 추가

--- 1-cube/main.js
+++ 1-cube/main.js
@@ -1,10 +1,13 @@
 import * as THREE from 'three';
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({
+  antialias: true, // 표면이 덜 매끄러운 현상을 고쳐줌
+});
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene(); // 장면
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -12,5 +15,15 @@ const camera = new THREE.PerspectiveCamera(
   1,
   500
 );
+
+const geometry = new THREE.BoxGeometry(2, 2, 2); // 도형
+const meterial = new THREE.MeshBasicMaterial({ color: 'blue' }); // 재질
+
+const cube = new THREE.Mesh(geometry, meterial); // 도형과, 재질을 받아 오브젝트 생성
+
+scene.add(cube);
+
+camera.position.set(3, 2, 5);
+camera.lookAt(cube.position); // 물체의 포지션을 넣어주면 잘 보이게 카메라 포지션을 잡음
 
 renderer.render(scene, camera);
