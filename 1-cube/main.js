d1eae4f7293beccd026ea322e351407d14e0648e
SeungYn <kwls0407@gmail.com>
2023-08-19 09:49:03 +0900

    part-3-2-10 renderer 리사이징

--- 1-cube/main.js
+++ 1-cube/main.js
@@ -17,7 +17,14 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 const geometry = new THREE.BoxGeometry(2, 2, 2); // 도형
-const meterial = new THREE.MeshStandardMaterial({ color: 'blue' }); // 재질
+const meterial = new THREE.MeshStandardMaterial({
+  color: 'blue',
+  // transparent: true, // 투명 설정
+  // opacity: 0.5, // 투명도 조절
+  //visible: false,
+  //wireframe: true, // 뼈대만 남게해줌
+  // side: THREE.DoubleSide, // 어디를 렌더링할찌, 육면체 외부, 내부, 양면
+}); // 재질
 
 const cube = new THREE.Mesh(geometry, meterial); // 도형과, 재질을 받아 오브젝트 생성
 
@@ -35,3 +42,12 @@ ambientLight.position.set(3, 2, 1);
 
 scene.add(ambientLight);
 renderer.render(scene, camera);
+
+function handleResize() {
+  camera.aspect = window.innerWidth / window.innerHeight; // 카메라 종횡비
+  camera.updateProjectionMatrix(); // 브라우져에 적용시키려면 호출해야함
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', handleResize);
