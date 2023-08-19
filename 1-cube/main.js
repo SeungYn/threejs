ae6b818d397d8951c8f66a81eebb606116fd2974
SeungYn <kwls0407@gmail.com>
2023-08-19 11:25:47 +0900

    part-3-2-13 Oribit controls

--- 1-cube/main.js
+++ 1-cube/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const renderer = new THREE.WebGLRenderer({
   antialias: true, // 표면이 덜 매끄러운 현상을 고쳐줌
@@ -15,6 +16,22 @@ const camera = new THREE.PerspectiveCamera(
   1,
   500
 );
+
+const controls = new OrbitControls(camera, renderer.domElement); // OrbitControls추가
+controls.autoRotate = true; // 카메라를 자동으로 회전 시켜줌
+// controls.autoRotateSpeed = 30; //속도 지정
+controls.enableDamping = true; // 스케이팅 효과
+controls.dampingFactor = 0.01; // 관성 제어
+controls.enableZoom = true; // 카메라 줌 가능하게함
+controls.enablePan = true; // 카메라를 좌우로 움직임
+//controls.maxDistance = 50; // 최대 거리 설정
+//controls.minDistance = 10; // 최소 거리 설정
+//controls.maxPolarAngle = Math.PI / 2; // 카메라 회전 최대 각도
+//controls.minPolarAngle = Math.PI / 2; // 카메라 회전 최소 각도
+
+//const axisHelper = new THREE.AxesHelper(5); // x,y,z 축 생성
+
+//scene.add(axisHelper);
 
 const cubeGeometry = new THREE.IcosahedronGeometry(1); // 도형
 const cubeBeterial = new THREE.MeshLambertMaterial({
@@ -60,15 +77,18 @@ reunder();
 function reunder() {
   //cube.rotation.x += THREE.MathUtils.degToRad(45);
   //cube.rotation.x = Date.now() / 1000; // 시간을 이용해서 어떤 프레임 환경에서 실행하든 같은 시간에 동일한 결과를 보게해줌
-  cube.rotation.x = clock.getElapsedTime(); // 클락인스턴스가 생성된 시점 이후 초단위 경과 시간
-  cube.rotation.y = clock.getElapsedTime();
+  // cube.rotation.x = clock.getElapsedTime(); // 클락인스턴스가 생성된 시점 이후 초단위 경과 시간
+  // cube.rotation.y = clock.getElapsedTime();
 
-  skeleton.rotation.x = clock.getElapsedTime() * 1.5;
-  skeleton.rotation.y = clock.getElapsedTime() * 1.5;
+  // skeleton.rotation.x = clock.getElapsedTime() * 1.5;
+  // skeleton.rotation.y = clock.getElapsedTime() * 1.5;
   // cube.position.y = Math.sin(cube.rotation.x); // 위아래로 움직이게 해줌
   // cube.scale.x = Math.acos(cube.rotation.x); // 큐브 사이즈 변경
 
   //cube.rotation.z += 0.01;
+
+  controls.update(); // controls도 카메라와 마찬가리로 변경시키면 update를 호출해야됨
+
   renderer.render(scene, camera);
   requestAnimationFrame(reunder);
 }
