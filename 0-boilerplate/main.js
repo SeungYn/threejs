e1c9e4c94889a1babd7de0534c2498035d2e1317
SeungYn <kwls0407@gmail.com>
2023-08-19 11:34:34 +0900

    보일러 플레이트 코드 수정

--- 0-boilerplate/main.js
+++ 0-boilerplate/main.js
@@ -1,24 +1,37 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const renderer = new THREE.WebGLRenderer({
+  antialias: true, // 표면이 덜 매끄러운 현상을 고쳐줌
+});
 
-setupCounter(document.querySelector('#counter'))
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene(); // 장면
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  1,
+  500
+);
+
+scene.add(cube, skeleton);
+
+camera.position.set(0, 0, 5);
+
+reunder();
+
+function reunder() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(reunder);
+}
+
+function handleResize() {
+  camera.aspect = window.innerWidth / window.innerHeight; // 카메라 종횡비
+  camera.updateProjectionMatrix(); // 브라우져에 적용시키려면 호출해야함
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', handleResize);
