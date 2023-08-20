a822dcffec538a792437b993d3695df757c850ea
SeungYn <kwls0407@gmail.com>
2023-08-20 22:58:13 +0900

    gsap를 이용한 애니메이션 적용

--- 3-interative-card/main.js
+++ 3-interative-card/main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Card from './Card';
 import { GUI } from 'lil-gui';
+import { gsap } from 'gsap';
 
 const gui = new GUI();
 
@@ -68,6 +69,13 @@ const ambientLight = new THREE.AmbientLight('white', 0.8);
 ambientLight.position.set(-5, -5, -5);
 scene.add(ambientLight);
 
+//애니메이션 추가 인자로 원하는 속성과 적용시킬 속성 명을 객체로 넣음
+gsap.to(card.mesh.rotation, {
+  y: -Math.PI * 4,
+  duration: 2.5,
+  ease: 'back.out(4)',
+});
+
 // 직사 광선 추가
 
 const directionalLight1 = new THREE.DirectionalLight(0xffffff, 0.6);
@@ -100,6 +108,11 @@ COLORS.forEach((color) => {
 
   button.addEventListener('click', () => {
     card.mesh.material.color = new THREE.Color(color); // 속성으로 변환시 THREE.Color 인스턴스로 색상을 변경시킬 수 있음
+    gsap.to(card.mesh.rotation, {
+      y: card.mesh.rotation.y - Math.PI / 2,
+      duration: 1,
+      ease: 'back.out(4)',
+    });
   });
 
   button.style.backgroundColor = color;
